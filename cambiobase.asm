531b4ca0727fb586b5577da80148fa83b1fcb9e6
carlosbollero <carlosmbollero@gmail.com>
2016-06-01 16:52:14 -0300

    Agrega comentarios

--- cambiobase.asm
+++ cambiobase.asm
@@ -1,3 +1,17 @@
+;*************************************************************************
+; 75.03 ORGANIZACION DEL COMPUTADOR                                      *
+;                                                                        *
+;                   Intel 80x86 Packed Decimal (II)                      *
+; Se pide desarrollar un programa en assembler Intel 80x86 que lea       *
+; desde un archivo registros de 4 bytes que representan la configuración *
+; binaria de números almacenados en formato BCD (decimal  empaquetado).  *
+; Deberá permitir visualizar por pantalla su configuración octal y además*
+; el número almacenado, en base 10.                                      *
+; Nota: no es correcto generar el archivo con un editor de textos de     *
+; forma tal que cada registro sea una tira de 16 caracteres de los rangos*
+; 0..9 y A..F. Se aconseja el uso de un editor hexadecimal.              *
+;*************************************************************************
+
 segment codigo code
 ..start:
          mov   ax,datos
@@ -62,6 +76,8 @@ procesar:
          call  binAOctal
          ret
 
+; Carga el registro de como si fuera Big-Endian,
+; entonces carga 1234567F en lugar de cargar 7F563412
 cargar:
          mov   si,0
          mov   edx,0
